ccc6929bd5ea373d5a3bb08eac98a9d23652fa68
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-11 10:25:12 +0000

    props: learnt how to pass props with the right type. And also dealt with children props and the propsWithChildren

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import Component from "./starter/01-return";
+
+import Component from "./starter/02-props";
 
 function App() {
   return (
     <main>
-      <Component/>
+      <Component name='Abdul-Mumin Rufai' school='University for Development Studies' id={57}>
+        <h2>this is Rufai</h2>
+      </Component>
     </main>
   );
 }
--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,8 +1,20 @@
-function Component() {
+import {type PropsWithChildren } from "react";
+
+type myProp = PropsWithChildren<{ 
+  name: string;
+  school: string;
+  id: number;
+}>
+
+function Component({ name, school, id, children}: myProp) {
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Props</h2>
+      <h2>name: {name}</h2>
+      <h2>school:{school}</h2>
+      <h3>ID: {id}</h3>
+      {children}
     </div>
   );
 }
