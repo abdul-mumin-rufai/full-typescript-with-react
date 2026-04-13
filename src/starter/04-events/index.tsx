05b3d313852823f351955fc4d5218980c68cf483
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-13 22:02:28 +0000

    events: handled form submit and how to get form data

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,24 +1,39 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function Component() {
   const [text, setText] = useState('');
   const [email, setEmail] = useState('');
+
+  interface EventPerson { name:string};
 
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
     setEmail(e.target.value);
   } // but we have to explicity set the type if we use a function as reference
 
+  // handling form data
+  const handleForm = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //const formData = new FormData(e.currentTarget);
+    const formData = new FormData(e.target as HTMLFormElement);
+    //const data = Object.fromEntries(formData);
+    //console.log(data);
+    const getText = formData.get('email') as string; // gocha getting the form data can be null so we use type assertion
+    const eventPerson: EventPerson = { name: getText };
+    console.log(eventPerson);
+    
+  }
+
   return (
     <div>
       <section>
-        <form className="form">
+        <form onSubmit={handleForm} className="form">
           <input
             className="form-input mb-1"
             type="text"
             name="text"
             value={text}
-            onChange={(e) => setText(e.target.value)} //if we passed the evebts inline typescript infer the type of the type for us
+            onChange={(e) =>  setText(e.target.value)} //if we passed the evebts inline typescript infer the type of the type for us
           />
           <input
             className="form-input mb-1"
