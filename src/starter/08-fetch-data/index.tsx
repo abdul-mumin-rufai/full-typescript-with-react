d9df850b0dd52c44883cd72bd57b52f01c339f09
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-20 20:19:22 +0000

    fetchData: fetech data with react using useEffect

--- src/starter/08-fetch-data/index.tsx
+++ src/starter/08-fetch-data/index.tsx
@@ -1,4 +1,38 @@
+import { useState, useEffect } from 'react';
+const url = 'https://www.course-api.com/react-tours-project';
+
 function Component() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`failed to fetach data...`);
+        }
+        const rawData = response.json();
+        console.log(rawData);
+        
+      } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'there was an error';
+        setIsError(errorMessage);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (
+    isLoading) {
+    return <h2>Loading...</h2>
+  };
+  if (isError) {
+    return <h2>Error...</h2>
+  }
   return (
     <div>
       <h2>React & Typescript</h2>
