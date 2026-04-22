7688104fee78c3af8da24f37fd70501909a5cd3f
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-22 19:00:18 +0000

    redux: set up the counterSlice and created it reducer for the logic

---
+++ src/starter/09-rtk/counterSclice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { type PayloadAction } from "@reduxjs/toolkit";
+
+
+type StatusAction = 'pending...'|'active'| 'notActive'
+
+interface CountState { 
+    count: number;
+    status: StatusAction
+};
+
+export const initialState:CountState = {
+    count: 0,
+    status: 'pending...'
+}
+
+export const CounterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+            state.count += 1;
+        },
+        decrement: (state) => {
+            state.count -= 1;
+        },
+        reset: (state) => {
+            state.count = 0;
+        },
+        stateStatus: (state, action:PayloadAction<StatusAction>) => {
+            state.status= action.payload
+        },
+    }
+});
