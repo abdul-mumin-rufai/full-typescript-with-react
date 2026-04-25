26e8f3b0fea8f3963005543311b336758cfb3914
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-25 22:03:18 +0000

    tasksProject: added new tasks to the tasks list

--- src/starter/10-tasks/Form.tsx
+++ src/starter/10-tasks/Form.tsx
@@ -1,11 +1,44 @@
+import { useState } from "react"
+import {type Task } from "./types";
+
+type TaskProp = {
+    addTask: (task: Task) => void;
+};
+
+const Form = ({addTask}:TaskProp) => {
+    const [text, setText] = useState('');
 
 
-
-const Form = () => {
+    // we must set the type for the e ie the event
+    const handleSumbit = (e:React.FormEvent<HTMLFormElement>) => { 
+        e.preventDefault();
+        if (!text) {
+            alert('please enter a text value')
+            return;
+        }
+        // add tasks
+        addTask({
+            id: new Date().getDate().toString(),
+            description: text,
+            isComplete:false,
+        });
+    
+        setText('');
+    };
 
   return (
-    <div>form</div>
+      <section>
+          <form className="form task-form" onSubmit={handleSumbit}>
+              <input
+                  type="text"
+                  name="form-input"
+                  value={text}
+                  onChange={(e) => { setText(e.target.value) }} // we didn't provide the type here because typescript an infer inline passing
+              />
+              <button type="submit" className="btn">add task</button>
+          </form>
+    </section>
   )
 }
 
-export default Form
+export default Form;
--- src/starter/10-tasks/index.tsx
+++ src/starter/10-tasks/index.tsx
@@ -1,12 +1,22 @@
 import Form from "./form";
 import List from "./list";
+import { useState } from "react";
+import { type Task } from "./types";
 
 function Component() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (task: Task): void => { 
+    setTasks([...tasks, task])
+  };
+  console.log(tasks);
+  
+  
   return (
-    <div>
-      <Form />
+    <section>
+      <Form addTask={addTask}/>
       <List/>
-    </div>
+    </section>
   );
 }
 export default Component;
